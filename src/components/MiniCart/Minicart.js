3475d21418149ca108f6dca46a810354fec6b035
i-Lene <marlene.s.veiga@gmail.com>
2025-02-10 23:29:50 +0000

    adding modal

--- src/components/MiniCart/Minicart.js
+++ src/components/MiniCart/Minicart.js
@@ -1,29 +1,43 @@
 "use client";
 import Modal from "../Modal/Modal";
 import { useCart } from "@/context/CartContext";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faX } from "@fortawesome/free-solid-svg-icons";
 
 export default function Minicart({ onClose }) {
   const { cartItems, removeFromCart } = useCart();
 
   return (
     <Modal onClose={onClose}>
-      <div>
+      <div className="modal-inner-wrapper">
         <h2>Minicart</h2>
+        <button className="btn-close" onClick={onClose}>
+          <FontAwesomeIcon icon={faX} />
+        </button>
         {cartItems.length === 0 ? (
-          <p>Your cart is empty</p>
+          <p className="emptyCart">Your cart is empty</p>
         ) : (
           cartItems.map((item) => (
-            <div key={item.id}>
-              <h3>{item.title}</h3>
-              <p>{item.price} EUR</p>
-              <p>Quantity: {item.quantity}</p>
-              <button onClick={() => removeFromCart(item.id)}>Remove</button>
+            <div key={item.id} className="minicart-item">
+              <div className="item-pic">
+                <img src={item.image} />
+              </div>
+              <div className="minicart-item-details">
+                <h3>{item.title}</h3>
+                <p>{item.price} EUR</p>
+                <p>Quantity: {item.quantity}</p>
+                <button
+                  className="remove-prod-btn"
+                  onClick={() => removeFromCart(item.id)}
+                >
+                  Remove
+                </button>
+              </div>
             </div>
           ))
         )}
-        <button onClick={onClose}>Close</button>
       </div>
+      <button className="btn btn-primary primary goTo">Go To Checkout</button>
     </Modal>
   );
 }
-
